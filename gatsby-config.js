9e0269a3bd8134986cd3b69a2c82e3f95fe39793
kwa <kwa@1f8.dev>
2020-09-06 13:25:13 +0900

    Create from gatsby starter blog template; Add cool icon; Update some metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `1F8 Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `1F8`,
+      summary: `ウェブ大工人`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `ウェブ開発について`,
+    siteUrl: `https://blog.1F8.dev/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `1f8`,
     },
   },
   plugins: [
@@ -60,13 +60,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `1F8 Blog`,
+        short_name: `1F8Blog`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/icon512.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
